bd8c8b642dda70cf9f3066e0f89be9c0a2a42cab
patrick-gordon <patrickgordon42@yahoo.com>
2020-01-07 19:26:46 -0600

    MVP

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -19,13 +19,27 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req,res) => { 
+    // select * from accounts where id = req.params.id 
+    knex
+    .select('*')
+    .from('accounts')
+    .where({id: req.params.id})
+    .first()
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ errorMessage: 'error woth getting account by ID'})
+    })
+})
 
 router.post('/', (req,res) => {
     const accountData = req.body;
     //insert () into values
     knex('accounts')
     .insert(accountData, 'id')
-    .first()
     .then(ids => {
         const id = ids[0];
         res.status(200).json(id)
@@ -33,7 +47,38 @@ router.post('/', (req,res) => {
     .catch(err => {
         console.log(err)
     res.status(500).json({ errorMessage: 'error creating account' })
+    })
 })
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    knex('accounts')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+        res.status(200).json({ updated: count })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({message: 'ERROR updating'})
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    knex('accounts')
+    .where({ id })
+    .del()
+    .then(count => {
+        res.status(200).json({deleted: count})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'error deleting'})
+    })
 })
 
 
